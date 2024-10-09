e3dd297e8cf8c8cccdac1aace7315582b1ea76d9
Eduardo <146763563+eduardo-mw@users.noreply.github.com>
2024-10-09 16:50:57 -0700

    add players to entrypoint

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 import { Hono } from "hono";
+import players from "./routes/players.ts";
 
 const app = new Hono();
 
 app.get("/health", (c) => {
   return c.json({ "status": "up" });
 });
+
+// Routes
+app.route("/players", players);
 
 Deno.serve(app.fetch);
