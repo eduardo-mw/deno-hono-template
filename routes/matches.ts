1b40d327f0d586ec265169614bd13de549647a37
Eduardo <146763563+eduardo-mw@users.noreply.github.com>
2024-10-14 20:50:04 -0700

    matches err handling

--- routes/matches.ts
+++ routes/matches.ts
@@ -4,21 +4,49 @@ const matches = new Hono();
 
 // GET all matches
 matches.get("/", async (c) => {
-  const allMatches = await MatchModel.find();
+  let allMatches;
+
+  try {
+    allMatches = await MatchModel.find();
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({ error: error.message });
+    }
+  }
+
   return c.json(allMatches);
+});
+
+// GET match by id
+matches.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  let singleMatch;
+
+  try {
+    singleMatch = await MatchModel.findById(id);
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({ error: error.message });
+    }
+  }
+
+  return c.json(singleMatch);
 });
 
 // Create a new match
 matches.post("/", async (c) => {
   const body = await c.req.json<IMatch>();
+  let newMatch;
+
   try {
-    const newMatch = await MatchModel.create(body);
-    return c.json(newMatch);
+    newMatch = await MatchModel.create(body);
   } catch (error) {
     if (error instanceof Error) {
-      return c.json({ "error": error });
+      return c.json({ error: error.message });
     }
   }
+
+  return c.json(newMatch);
 });
 
 // Update a match
